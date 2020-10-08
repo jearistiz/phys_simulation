74a1b1b235898f283bfcade4d56527d99bbd93f2
Juan Aristizabal-Zuluaga <jeaz.git@gmail.com>
2020-10-08 15:49:50 -0500

    added .readthedocs.yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,8 +25,8 @@ copyright = '2020, Juan E. Aristizabal'
 author = 'Juan E. Aristizabal'
 
 # The full version, including alpha/beta/rc tags
-version = '0.0'
-release = '0.0'
+version = '1.0'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
